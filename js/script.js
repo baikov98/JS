76a01047d699fd91fd782680636a8b3d24e5bbae
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2020-10-28 16:34:33 +0500

    update func

--- js/script.js
+++ js/script.js
@@ -12,8 +12,28 @@ class Sprite {
         this.image = options.image;
         this.width = options.width;
         this.height = options.height;
+        this.render();
+        this.frameIndex = 0;
+        this.tickCount = 0;
+        this.ticksPerFrame = options.ticksPerFrame || 0;
+        this.update()
+        this.numberOfFrames = options.numberOfFrames || 1;
+        update() {
+            this.tickCount++;
+            if (this.tickCount > this.ticksPerFrame) {
+                
+            }
+        }
     }
 }
 
+let sprite = new Sprite ({
+    ctx: CanvasGradient.getContext('2d'),
+    image: ric1,
+    width: 110,
+    height: 60,
+    numberOfFrames: 10,
+    ticksPerFrame: 4,
+})
 
 
